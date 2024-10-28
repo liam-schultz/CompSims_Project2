5d17c4406c92fbe185b0d8c94654973bfb03cfc3
Liam Schultz <lschult7@uwo.ca>
2024-10-28 14:13:01 -0400

    implemented the Euler-Cromer and Midpoint methods. Tested by plotting fig 2.3 for Euler-Cromer and Midpoint. Added a title to fig 2.2

--- SchultzLiam_Project2.py
+++ SchultzLiam_Project2.py
@@ -18,7 +18,7 @@ def integrate(inital_speed, launch_angle, time_step, method, air_resistance=True
 
     #extract the components of the initial velocity and set the initial position
     x_array, y_array = [], []
-    x, y = 0, 0
+    x, y = 0, 1
     v_x, v_y = inital_speed*np.cos(launch_angle), inital_speed*np.sin(launch_angle)
     while y >= 0:
         x_array.append(x)
@@ -30,20 +30,46 @@ def integrate(inital_speed, launch_angle, time_step, method, air_resistance=True
             a = (0, -g)
 
         if method == "euler":
-            x = x+time_step*v_x
-            y = y+time_step*v_y
-            v_x = v_x+time_step*a[0]
-            v_y = v_y+time_step*a[1]
+            x = x + time_step*v_x
+            y = y + time_step*v_y
+            v_x = v_x + time_step*a[0]
+            v_y = v_y + time_step*a[1]
+        elif method == "euler-cromer":
+            v_x = v_x + time_step*a[0]
+            v_y = v_y + time_step*a[1]
+            x = x + time_step*v_x
+            y = y + time_step*v_y
+        elif method == "midpoint":
+            v_nx = v_x
+            v_ny = v_y
+            v_x = v_x + time_step*a[0]
+            v_y = v_y + time_step*a[1]
+            x = x + time_step*((v_x+v_nx)/2)
+            y = y + time_step*((v_y+v_ny)/2)
     x_array.append(x)
     y_array.append(y)
 
-    #code for plotting fig 2.2 (also set y to 0 above)
+    """#code for plotting fig 2.2 (also set y to 0 above)
     plt.scatter(x_array, y_array, marker="P")
     plt.xlabel("Range (m)")
     plt.ylabel("Height (m)")
+    plt.title("Projectile motion")
     plt.xlim(0, 25)
     plt.ylim(-1, 7)
+    plt.show()"""
+
+    #code for plotting fig 2.3
+    plt.scatter(x_array, y_array, marker="P")
+    plt.xlabel("Range (m)")
+    plt.ylabel("Height (m)")
+    plt.title("Projectile motion")
+    plt.xlim(0, 250)
+    plt.ylim(-10, 70)
     plt.show()
 
+"""#function call for plotting fig 2.2
 integrate(15, 45, 0.1, "euler", air_resistance=False)
+"""
 
+#function call for plotting fig 2.3 (change method parameter to euler-cromer or midpoint)
+integrate(50, 45, 0.1, "midpoint", air_resistance=True)
